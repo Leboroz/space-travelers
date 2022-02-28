c75406ef0b8142ed9c90223fccb157a1edfa3342
Bondok6 <kirolousbondok6@gmail.com>
2022-02-28 22:49:55 +0200

    Fix stylelint error by adding index.css file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
