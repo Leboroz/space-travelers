ab020b2ee6efd4db7c644815964143ac80dd2168
Leonardo <albornozleo95@gmail.com>
2022-03-01 17:01:14 -0400

    Add Structure
    I created a card component importing Card from React-bootstrap. Then, I
    created a list component that maps the fetched API object into card
    component.

--- src/components/rockets/Rockets.jsx
+++ src/components/rockets/Rockets.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import RocketCard from './Card';
 
-const Rockets = () => <>rockets</>;
+const Rockets = ({ list }) => {
+  return (
+    <div className="row">
+      {list.map((rocket) => (
+        <div className="col-sm-6 col-lg-12 mb-3">
+          <RocketCard key={rocket.id} props={rocket} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Rockets;
