1bdf9d79a0368314860d33cea843001b8def80ac
Leonardo <albornozleo95@gmail.com>
2022-03-02 11:07:14 -0400

    Fix bug in rocket page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import MainHeader from './components/MainHeader';
 import MissionsPage from './pages/MissionsPage';
 import RocketsPage from './pages/RocketsPage';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import initState from '../redux/rockets/rocketsAPI';
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.rockets.rocketsData);
+
+  useEffect(() => {
+    dispatch(initState());
+  }, [dispatch]);
   return (
     <>
       <header>
@@ -12,7 +22,7 @@ function App() {
 
       <main className="container my-3">
         <Routes>
-          <Route path="/" element={<RocketsPage />} />
+          <Route path="/" element={<RocketsPage state={state} />} />
           <Route path="/missions" element={<MissionsPage />} />
         </Routes>
       </main>
--- src/pages/RocketsPage.jsx
+++ src/pages/RocketsPage.jsx
@@ -1,17 +1,6 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
 import Rockets from '../components/rockets/Rockets';
-import initState from '../redux/rockets/rocketsAPI';
 
-const RocketsPage = () => {
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state.rockets.rocketsData);
-
-  useEffect(() => {
-    dispatch(initState());
-  }, [dispatch]);
-
+const RocketsPage = ({ state }) => {
   return <Rockets list={state} />;
 };
 
