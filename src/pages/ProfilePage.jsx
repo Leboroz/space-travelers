81d7f640485d21c800b2ac7447404a8852b5a51e
Bondok6 <kirolousbondok6@gmail.com>
2022-03-02 18:14:15 +0200

    Add filter missions functionality

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+
 const ProfilePage = () => {
+  const state = useSelector((state) => state);
+
+  const { missions, rockets } = state;
+
+  console.log(missions, rockets);
+
+  const getMissions = () => {
+    const joinedMissions = missions.missionsData.filter(
+      (mission) => mission.reserved
+    );
+
+    if (joinedMissions.length === 0) {
+      return <li>There is No Missions</li>;
+    }
+    return joinedMissions.map((mission) => (
+      <li key={mission.mission_id}>{mission.mission_name}</li>
+    ));
+  };
+
   return (
     <>
       <Row>
@@ -16,13 +37,7 @@ const ProfilePage = () => {
         </Col>
         <Col>
           <h2> My Missions </h2>
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-          </ul>
+          <ul>{getMissions()}</ul>
         </Col>
       </Row>
     </>
