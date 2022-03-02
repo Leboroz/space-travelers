668629766dd0718851246458178bc107c0190ea7
Bondok6 <kirolousbondok6@gmail.com>
2022-03-02 17:43:18 +0200

    Add Basic Layout to profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
-
+import { Row, Col } from 'react-bootstrap';
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <>
+      <Row>
+        <Col>
+          <h2>My Rockets</h2>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+          </ul>
+        </Col>
+        <Col>
+          <h2> My Missions </h2>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+          </ul>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProfilePage;
